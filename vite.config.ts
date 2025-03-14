cec276fe7a9b44cbcfad11d840bad6cfaef4a8fc
Estil1 <100552136@est.uasd.edu.do>
2025-03-14 11:27:17 -0400

    arreglado el bug de la pagina

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Trap',
   server: {
     proxy: {
       '/api': 'http://localhost:3000'
