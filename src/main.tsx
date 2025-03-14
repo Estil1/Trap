ffc8c33e25a239dee8f349949c7ffcc7f0dd4fba
Estil1 <100552136@est.uasd.edu.do>
2025-03-14 11:33:11 -0400

    cambiso url

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,8 @@ import Privacy from './Privacy.tsx'
 import './index.css'
 
 // Determine which component to render based on the URL path
-const path = window.location.pathname.replace('/Trap', '')
-const Component = path === '/privacy' ? Privacy : App
+const path = window.location.pathname.replace('/Trap/', '').replace('/Trap', '')
+const Component = path === 'privacy' ? Privacy : App
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Trap/',
   server: {
     proxy: {
       '/api': 'http://localhost:3000'
